2aba6be8fb8280ba54c4e54b21f0ba5a820be5f8
Marcos <marqtrind@gmail.com>
2019-08-02 20:13:12 -0300

    segundo commit

---
+++ src/SistemaEmpresarial/Empresa.java
@@ -0,0 +1,28 @@
+
+package SistemaEmpresarial;
+
+
+public class Empresa {
+    public String NomeEmpresa;
+    public int cnpj;
+
+    public String getNomeEmpresa() {
+        return NomeEmpresa;
+    }
+
+    public void setNomeEmpresa(String NomeEmpresa) {
+        this.NomeEmpresa = NomeEmpresa;
+    }
+
+    public int getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(int cnpj) {
+        this.cnpj = cnpj;
+    }
+
+   
+    }
+
+    
